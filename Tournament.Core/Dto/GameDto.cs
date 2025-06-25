08b776900123f46b357d31ce279e5133a97dc3af
davarng <113583854+davarng@users.noreply.github.com>
2025-06-25 14:55:50 +0200

    Add validation attributes to GameDto and TournamentDto
    Updated GameDto and TournamentDto classes to include data validation attributes for the Title property. Added [Required], [MinLength], and [MaxLength] attributes to enforce validation rules. Simplified class structure by removing unnecessary braces and indentation while retaining existing properties.

--- Tournament.Core/Dto/GameDto.cs
+++ Tournament.Core/Dto/GameDto.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tournament.Core.Dto
+namespace Tournament.Core.Dto;
+
+public class GameDto
 {
-    public class GameDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime StartDate { get; set; }
-    }
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required.")]
+    [MinLength(2, ErrorMessage = "Title is too short(Min 2 chars).")]
+    [MaxLength(50, ErrorMessage = "Title is too long(Max 50 chars).")]
+    public string Title { get; set; }
+    public DateTime StartDate { get; set; }
 }
