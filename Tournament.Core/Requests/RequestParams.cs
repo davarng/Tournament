d30b2d388a2ac40faaacbdab20ba100cdefa65fa
davarng <113583854+davarng@users.noreply.github.com>
2025-07-08 16:30:16 +0200

    Added requestparams

---
+++ Tournament.Core/Requests/RequestParams.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Requests
+{
+    public class RequestParams
+    {
+        [Range(1, int.MaxValue)]
+        public int PageNumber { get; set; } = 1;
+
+        [Range(2, 20)]
+        public int PageSize { get; set; } = 5;
+    }
+
+    public class TournamentRequestParams : RequestParams
+    {
+        public bool IncludeGames { get; set; } = false;
+    }
+}
