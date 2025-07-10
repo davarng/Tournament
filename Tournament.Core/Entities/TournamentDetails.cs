07a820959f388a9c818ea783c6af5a06ae4dbd3e
davarng <113583854+davarng@users.noreply.github.com>
2025-07-10 17:05:17 +0200

    small edit tournamentdetails

--- Tournament.Core/Entities/TournamentDetails.cs
+++ Tournament.Core/Entities/TournamentDetails.cs
@@ -7,5 +7,5 @@ public class TournamentDetails
     public int Id { get; set; }
     public string Title { get; set; }
     public DateTime StartDate { get; set; }
-    public virtual ICollection<Game> Games { get; set; }
+    public virtual ICollection<Game> Games { get; set; } = new HashSet<Game>();
 }
