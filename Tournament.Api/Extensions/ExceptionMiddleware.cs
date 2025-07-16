8ea84a6451194b2a7806272cbf5ae81f2da28b9f
davarng <113583854+davarng@users.noreply.github.com>
2025-07-16 16:03:39 +0200

    Custom default exception test

--- Tournament.Api/Extensions/ExceptionMiddleware.cs
+++ Tournament.Api/Extensions/ExceptionMiddleware.cs
@@ -8,6 +8,45 @@ namespace Tournament.Api.Extensions;
 public static class ExceptionMiddleware
 {
 
+    //public static void ConfigureExceptionHandler(this WebApplication app)
+    //{
+    //    app.UseExceptionHandler(appError =>
+    //    {
+    //        appError.Run(async context =>
+    //        {
+    //            var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
+    //            if (contextFeatures != null)
+    //            {
+    //                var problemDetailsFactory = app.Services.GetRequiredService<ProblemDetailsFactory>();
+    //                ArgumentNullException.ThrowIfNull(nameof(problemDetailsFactory));
+
+    //                var problemDetails = CreateProblemDetails(context, contextFeatures.Error, problemDetailsFactory, app);
+
+    //                context.Response.StatusCode = problemDetails.Status ?? StatusCodes.Status500InternalServerError;
+    //                await context.Response.WriteAsJsonAsync(problemDetails);
+    //            }
+    //        });
+    //    });
+    //}
+
+    //private static ProblemDetails CreateProblemDetails(HttpContext context, Exception error, ProblemDetailsFactory problemDetailsFactory, WebApplication app)
+    //{
+    //    return error switch
+    //    {
+    //        NotFoundException notFoundException => problemDetailsFactory.CreateProblemDetails(
+    //            context,
+    //            StatusCodes.Status404NotFound,
+    //            title: notFoundException.Title,
+    //            detail: notFoundException.Message,
+    //            instance: context.Request.Path),
+
+    //        _ => problemDetailsFactory.CreateProblemDetails(
+    //            context,
+    //            StatusCodes.Status500InternalServerError,
+    //            title: "Internal Server Error",
+    //            detail: app.Environment.IsDevelopment() ? error.Message : "Un unexpected error occured.")
+    //    };
+    //}
     public static void ConfigureExceptionHandler(this WebApplication app)
     {
         app.UseExceptionHandler(appError =>
@@ -33,12 +72,12 @@ public static class ExceptionMiddleware
     {
         return error switch
         {
-            NotFoundException notFoundException => problemDetailsFactory.CreateProblemDetails(
-                context,
-                StatusCodes.Status404NotFound,
-                title: notFoundException.Title,
-                detail: notFoundException.Message,
-                instance: context.Request.Path),
+            ApiException apiEx => problemDetailsFactory.CreateProblemDetails(
+            context,
+            apiEx.StatusCode,
+            title: apiEx.Title,
+            detail: apiEx.Message,
+            instance: context.Request.Path),
 
             _ => problemDetailsFactory.CreateProblemDetails(
                 context,
---
+++ Tournament.Core/Exceptions/ApiException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; }
+        public string Title { get; }
+
+        public ApiException(int statusCode, string title, string message) : base(message)
+        {
+            StatusCode = statusCode;
+            Title = title;
+        }
+    }
+}
